331f72cecb17e78ee2ab76f29666d2257b45d928
daggarwa81 <aggarwal.deepa@gmail.com>
2025-04-24 17:18:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] s) {
-        System.out.println("Hello Oracle - India Devops Session - Bangalore ");
+        System.out.println("Hello Deepa Oracle - India Devops Session - Bangalore ");
 
         for(int i = 0; i<=10; i++) {
             System.out.println(i);
